14cabb927d176472688040485c5d77e2007369c5
rajapc <abshar.abshar3726@gmail.com>
2022-06-11 11:35:51 +0530

    front page

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,6 +5,6 @@ from myapp import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('login', views.login, name='login'),
-     path('logout', views.logout, name='logout'),
+    path('logout', views.logout, name='logout'),
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,26 @@
+from pickle import NONE
 from django.shortcuts import render , redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import logout , authenticate , login
 
 # Create your views here.
 def index(request):
+    if request.user.is_anonymous:
+        return redirect("/login")
     return render (request, 'index.html')
-
+ 
 def login(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(username=username , password=password)
+        if user is not NONE:
+            login(user)
+            return redirect("/")
+        else:
+            return render (request, 'login.html')
     return render (request, 'login.html')
 
 def logout(request):
-    return render (request, 'index.html')
+    logout(request)
+    return redirect ("/login")
